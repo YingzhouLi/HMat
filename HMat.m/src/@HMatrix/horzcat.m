fa3951fcb9e1e4684e38327f396b60f070e7b3fe
YingzhouLi <lyzh588@gmail.com>
2017-02-14 11:54:24 -0800

    Simplified matrix cat

--- HMat.m/src/@HMatrix/horzcat.m
+++ HMat.m/src/@HMatrix/horzcat.m
@@ -2,10 +2,23 @@ function A = horzcat( varargin )
 
 height = varargin{1}.height;
 width = 0;
+merge = 0;
+wblock = 0;
 for it = 1:nargin
     width = width + varargin{it}.width;
+    if varargin{it}.blockType == 'H'
+        if size(varargin{it}.childHMat,1) == merge || merge == 0
+            merge = size(varargin{it}.childHMat,1);
+            wblock = wblock + size(varargin{it}.childHMat,2);
+        else
+            merge = -1;
+        end
+    else
+        merge = -1;
+    end
 end
 
+if merge == -1
 A = HMatrix;
 A.height = height;
 A.width = width;
@@ -13,6 +26,23 @@ A.blockType = 'H';
 A.childHMat = cell(1,nargin);
 for it = 1:nargin
     A.childHMat{it} = varargin{it};
+end
+else
+    A = HMatrix;
+    A.height = height;
+    A.width = width;
+    A.blockType = 'H';
+    A.childHMat = cell(merge,wblock);
+    offset = 0;
+    for it = 1:nargin
+        for iti = 1:merge
+            for itj = 1:size(varargin{it}.childHMat,2)
+                A.childHMat{iti,offset + itj} = ...
+                    varargin{it}.childHMat{iti,itj};
+            end
+        end
+        offset = offset + size(varargin{it}.childHMat,2);
+    end
 end
 
 end
--- HMat.m/src/@HMatrix/vertcat.m
+++ HMat.m/src/@HMatrix/vertcat.m
@@ -2,17 +2,47 @@ function A = vertcat( varargin )
 
 height = 0;
 width = varargin{1}.width;
+merge = 0;
+hblock = 0;
 for it = 1:nargin
     height = height + varargin{it}.height;
+    if varargin{it}.blockType == 'H'
+        if size(varargin{it}.childHMat,2) == merge || merge == 0
+            merge = size(varargin{it}.childHMat,2);
+            hblock = hblock + size(varargin{it}.childHMat,1);
+        else
+            merge = -1;
+        end
+    else
+        merge = -1;
+    end
 end
 
-A = HMatrix;
-A.height = height;
-A.width = width;
-A.blockType = 'H';
-A.childHMat = cell(nargin,1);
-for it = 1:nargin
-    A.childHMat{it} = varargin{it};
+if merge == -1
+    A = HMatrix;
+    A.height = height;
+    A.width = width;
+    A.blockType = 'H';
+    A.childHMat = cell(nargin,1);
+    for it = 1:nargin
+        A.childHMat{it} = varargin{it};
+    end
+else
+    A = HMatrix;
+    A.height = height;
+    A.width = width;
+    A.blockType = 'H';
+    A.childHMat = cell(hblock,merge);
+    offset = 0;
+    for it = 1:nargin
+        for iti = 1:size(varargin{it}.childHMat,1)
+            for itj = 1:merge
+                A.childHMat{offset + iti,itj} = ...
+                    varargin{it}.childHMat{iti,itj};
+            end
+        end
+        offset = offset + size(varargin{it}.childHMat,1);
+    end
 end
 
 end
